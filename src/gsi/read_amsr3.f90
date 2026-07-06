ce7a19c73be4d3398d653d49dbec04d91ac87f27
jianjunj <jianjun.jin@noaa.gov>
2026-07-06 16:20:27 +0000

    Check the subset name during reading amsr3 bufr data.

--- src/gsi/read_amsr3.f90
+++ src/gsi/read_amsr3.f90
@@ -284,6 +284,9 @@ integer(i_kind),dimension(npe)  ,intent(inout) :: nobs
   irec=0
   iobs=1
   do while(ireadmg(lnbufr,subset,idate)>=0)
+     if ( subset /= 'NC021248') then
+        cycle
+     endif
      irec=irec+1
      next=next+1
      if(next == npe_sub)next=0
