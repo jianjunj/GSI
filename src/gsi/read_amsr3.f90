e8871a17d5a3fb3bf7662acc3a083867b2087151
jianjunj <jianjun.jin@noaa.gov>
2026-07-09 15:57:44 +0000

    Clean up the lines to check subset names in read_amsr3.f90.

--- src/gsi/read_amsr3.f90
+++ src/gsi/read_amsr3.f90
@@ -279,11 +279,6 @@ integer(i_kind),dimension(npe)  ,intent(inout) :: nobs
   iobs=1
   do while(ireadmg(lnbufr,subset,idate)>=0)
 
-! j.jin, AMSR3 accidently saved into subset 'NC021247' which is for AMSR2 in NCEP BUFR.
-!    if ( subset /= 'NC021248') then
-!        cycle
-!    endif
-
      irec=irec+1
      next=next+1
      if(next == npe_sub)next=0
